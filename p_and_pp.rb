1a0e36b11723e2f68342e2c1240880b57a352075
Hideaki Tamai <tamadon3776@gmail.com>
2013-02-11 14:29:50 +0900

    P48 Column

---
+++ p_and_pp.rb
@@ -0,0 +1,13 @@
+#-*-encoding: utf-8-*-
+require "pp"
+v = [
+ {
+	:key_00 => "「たのしいRuby」",
+	:key_01 => "「Rubyレシビブック」",
+	:key_02 => "「Railsレシピブック」",
+	:key_03 => "「たのしいRuby 第３版」",
+ }
+    ]
+p v
+pp v
+
