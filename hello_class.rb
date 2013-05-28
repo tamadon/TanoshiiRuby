46d72e07ad6f5677f8605e79f3f55e17531f18c8
Hideaki Tamai <tamadon3776@gmail.com>
2013-05-28 17:16:06 +0900

    P124~126

---
+++ hello_class.rb
@@ -0,0 +1,18 @@
+class HelloWorld
+    Version = '1.0'
+
+    def initialize(myname="Ruby")
+      @name = myname
+    end
+
+    def hello
+      print "Hello, world. I am ", @name, ".\n"
+    end
+  end
+
+  bob   = HelloWorld.new("Bob")
+  alice = HelloWorld.new("Alice")
+  ruby  = HelloWorld.new
+
+  ruby.hello
+
